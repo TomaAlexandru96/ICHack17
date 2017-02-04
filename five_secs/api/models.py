89b39c02fac8a7ad38fead796f5b0c451605413d
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2017-02-04 21:42:28 +0000

    Token id -> user id

--- five_secs/api/models.py
+++ five_secs/api/models.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 class User(models.Model):
-    token_id = models.CharField(max_length=256, primary_key=True)
+    user_id = models.CharField(max_length=256, primary_key=True)
     name = models.CharField(max_length=256)
     picture_url = models.URLField()
 
 class Event(models.Model):
-    token_id = models.ForeignKey(User)
+    user_id = models.ForeignKey(User)
     rate = models.PositiveSmallIntegerField(null=True)
     video = models.FileField(null=True)
     description = models.TextField()
--- five_secs/api/serializers.py
+++ five_secs/api/serializers.py
@@ -4,10 +4,10 @@ from api.models import User, Event
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('token_id', 'name', 'picture_url')
+        fields = ('user_id', 'name', 'picture_url')
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ('id', 'token_id', 'rate', 'video', 'description', 'longitude', 'latitude')
+        fields = ('id', 'user_id', 'rate', 'video', 'description', 'longitude', 'latitude')
         read_only_fields = ('rate',  'video')
