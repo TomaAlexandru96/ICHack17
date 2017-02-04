d4ed27dc51a99ef8d98887f4f24aac2997b25d5d
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2017-02-04 17:51:13 +0000

    Make models

--- five_secs/api/models.py
+++ five_secs/api/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    token_id = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
+    picture_url = models.URLField()
+
+class Event(models.Model):
+    token_id = models.ForeignKey(User)
+    rate = models.PositiveSmallIntegerField()
+    video = models.FileField()
+    description = models.TextField()
