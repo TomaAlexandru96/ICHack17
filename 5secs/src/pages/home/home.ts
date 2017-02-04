3f2107cf47dda9cedf5b7f932c32b26614a65dd6
panpan2 <p.panayiotou2@gmail.com>
2017-02-04 13:53:24 +0000

    Display of map using MapBox

--- 5secs/src/pages/home/home.ts
+++ 5secs/src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+declare var L;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,6 +12,12 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+
+  }
+
+  ngOnInit() {
+    L.mapbox.accessToken = 'pk.eyJ1IjoiZmFuZ3lpIiwiYSI6ImNpeXI5dXBuZzAwMGszM3FudTQ3bG9tcDQifQ.25ONADCYigEjnEHUo0pRWg';
+    L.mapbox.map('map-one', 'mapbox.streets').setView([38.8929, -77.0252], 14);  
   }
 
 }
