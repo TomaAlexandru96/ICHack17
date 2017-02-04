5910051d8df7fc9029e9624057b799e225766155
panpan2 <p.panayiotou2@gmail.com>
2017-02-04 13:07:23 +0000

    Initial tabbed UI

--- 5secs/src/app/app.module.ts
+++ 5secs/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { ConnectivityService } from '../providers/connectivity-service';
+import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -15,8 +20,11 @@ import { ConnectivityService } from '../providers/connectivity-service';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, ConnectivityService]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
 export class AppModule {}
