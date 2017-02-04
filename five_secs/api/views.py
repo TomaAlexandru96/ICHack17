e58cf1ae572091da3f64ce9522703557fd0d7d24
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2017-02-04 20:42:33 +0000

    Allow any

--- five_secs/api/views.py
+++ five_secs/api/views.py
@@ -1,19 +1,24 @@
 from api.models import Event, User
 from api.serializers import EventSerializer, UserSerializer
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 class EventList(generics.ListCreateAPIView):
+    permission_classes = (AllowAny, )
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (AllowAny, )
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
 class UserList(generics.ListCreateAPIView):
+    permission_classes = (AllowAny, )
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (AllowAny, )
     queryset = User.objects.all()
     serializer_class = UserSerializer
