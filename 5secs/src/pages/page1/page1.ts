1f5d1639594712d8de073ec40589cdccd8ef56cc
panpan2 <p.panayiotou2@gmail.com>
2017-02-04 20:37:29 +0000

    Add sample events and plot them

--- 5secs/src/pages/page1/page1.ts
+++ 5secs/src/pages/page1/page1.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
 import { Geolocation } from 'ionic-native'
 import { NavController } from 'ionic-angular';
+import { HTTP } from 'ionic-native';
 
 declare var L;
 var map;
 var coordinates;
 var currentMarker = undefined;
+var events;
+/*
+var currentJson =
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-77.031952, 38.913184]
+    },
+    properties: {
+      'marker-color': '#ffa0d3',
+      'marker-size': 'large',
+    }
+  };
+var userJson =
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.031952, 38.913184]
+      },
+      properties: {
+        'marker-color': '#00ff0d3',
+        'marker-size': 'large',
+      }
+    };
+var eventJson =
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-77.031952, 38.913184]
+        },
+        properties: {
+          'marker-color': '#00f6ff',
+          'marker-size': 'large',
+        }
+      };*/
+
 
 @Component({
   selector: 'page-page1',
@@ -28,7 +68,32 @@ export class Page1 {
         resp.coords.longitude
       ];
       L.marker(coordinates).addTo(map); // What if you move?
-
+      // get all events in radius 1km
+      try {
+        //events = HTTP.post('server', {coordinates}, {});
+      } finally {
+        // mostly demo reasons, we don't want things to fail even at worst case at demo
+        events =
+          [{
+            name: "IC HACK",
+            lat: 51.49868324935443,
+            lng: -0.1772511005401611
+           },
+           {
+             name: "UNION BAR",
+             lat: 51.500368008689485,
+             lng: -0.17818450927734375
+           },
+           {
+              name: "COMPUTER LABS",
+              lat: 51.49875170957007,
+              lng: -0.17942905426025388
+           }];
+      }
+      // plot them
+      for (var e in events) {
+        L.marker([events[e].lat, events[e].lng]).addTo(map); // should store to remove later
+      }
     });
 
 
@@ -38,6 +103,7 @@ export class Page1 {
         currentMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
       } else {
         currentMarker.setLatLng(e.latlng);
+        console.log(e.latlng);
         currentMarker.update();
       }
     });
