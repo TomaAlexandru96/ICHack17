f3aecde7ad5ce8ce9e0bd77f8204c28c46040f5f
panpan2 <p.panayiotou2@gmail.com>
2017-02-04 15:08:21 +0000

    Locate user in map

--- 5secs/src/pages/home/home.ts
+++ 5secs/src/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import {Geolocation} from 'ionic-native'
 import { NavController } from 'ionic-angular';
 
 declare var L;
+var loc;
 
 @Component({
   selector: 'page-home',
@@ -12,12 +13,13 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
-
   }
 
   ngOnInit() {
     L.mapbox.accessToken = 'pk.eyJ1IjoiZmFuZ3lpIiwiYSI6ImNpeXI5dXBuZzAwMGszM3FudTQ3bG9tcDQifQ.25ONADCYigEjnEHUo0pRWg';
-    L.mapbox.map('map-one', 'mapbox.streets').setView([38.8929, -77.0252], 14);  
+    var map = L.mapbox.map('map-one', 'mapbox.streets').locate();
+    Geolocation.getCurrentPosition().then((resp) => {
+      map.setView([resp.coords.latitude, resp.coords.longitude], 14);
+    });
   }
-
 }
