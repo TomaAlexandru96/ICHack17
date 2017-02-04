338b09adad9146100c8b4964fa08f701cae94e20
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2017-02-04 22:50:32 +0000

    Gets photo and name

--- five_secs/api/views.py
+++ five_secs/api/views.py
@@ -2,6 +2,10 @@ from api.models import Event, User
 from api.serializers import EventSerializer, UserSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+import requests
 
 class EventList(generics.ListCreateAPIView):
     permission_classes = (AllowAny, )
@@ -13,12 +17,57 @@ class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
-class UserList(generics.ListCreateAPIView):
-    permission_classes = (AllowAny, )
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+@api_view(['GET', 'POST'])
+@permission_classes((AllowAny, ))
+def user_list(request):
+    """
+    List all users, or create a new user.
+    """
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (AllowAny, )
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    elif request.method == 'POST':
+        user_id = request.data["userID"]
+        token_id = request.data["accessToken"]
+        r = requests.get("https://graph.facebook.com/v2.8/me?access_token=%s&fields=id,name,picture" % token_id)
+        result = r.json()
+        try:
+            pic_url = result["picture"]["data"]["url"]
+        except ValueError:
+            return Response("You must have a profile picture", status=status.HTTP_400_BAD_REQUEST)
+        user = {
+            "user_id": user_id,
+            "picture_url": pic_url,
+            "name": result["name"]
+        }
+        if r.status_code != 200:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        serializer = UserSerializer(data=user)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT'])
+@permission_classes((AllowAny, ))
+def user_detail(request, pk):
+    """
+    Retrieve, update or delete a user instance.
+    """
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- five_secs/five_secs/urls.py
+++ five_secs/five_secs/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^events/$', views.EventList.as_view()),
     url(r'^events/(?P<pk>[0-9]+)/$', views.EventDetail.as_view()),
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^users/$', views.user_list),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.user_detail),
     url(r'^admin/', admin.site.urls),
 ]
