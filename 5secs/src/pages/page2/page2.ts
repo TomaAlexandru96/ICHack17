f04fd160aa05c673feab2284f7231b5aaa450ee7
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2017-02-05 08:22:51 +0000

    magic

--- 5secs/src/pages/page2/page2.ts
+++ 5secs/src/pages/page2/page2.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HTTP, MediaCapture, MediaFile, CaptureError, CaptureImageOptions } from 'ionic-native';
 import { NavController, NavParams } from 'ionic-angular';
+declare var FileTransfer;
+declare var FileUploadOptions;
 
 @Component({
   selector: 'page-page2',
@@ -16,15 +18,31 @@ export class Page2 {
   captureVideo() {
     MediaCapture.captureVideo({limit: 1, duration: 5})
     .then((response) => {
-      HTTP.uploadFile("http://13.74.168.159/upload_video/", {
-        event_id: this.navParams.data['id']
-      }, {}, response[0].fullPath, "video")
-      .then((serverResponse) => {
-        console.log(serverResponse);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+      console.log(JSON.stringify(response));
+      var win = function (r) {
+          console.log("Code = " + r.responseCode);
+          console.log("Response = " + r.response);
+          console.log("Sent = " + r.bytesSent);
+      }
+
+      var fail = function (error) {
+          alert("An error has occurred: Code = " + error.code);
+          console.log("upload error source " + error.source);
+          console.log("upload error target " + error.target);
+      }
+      var fileURL = encodeURI(response[0].localURL);
+      var options = new FileUploadOptions();
+      options.fileKey = "video";
+      options.fileName = fileURL.substr(fileURL.lastIndexOf('/') + 1);
+      options.mimeType = "text/plain";
+
+      var params = {
+          "event_id": this.navParams.data['id']
+      };
+      options.params = params;
+
+      var ft = new FileTransfer();
+      ft.upload(fileURL, encodeURI("http://13.74.168.159/upload_video/"), win, fail, options);
     })
     .catch((err) => {
       console.error(err);
