a9ca619b3763010ebf22988ee836000a13a0e87d
Andrei Isaila <ii515@ic.ac.uk>
2017-02-05 05:29:47 +0000

    Send video url to Microsoft Cognitive API and interpret the result by grading the video on a 1 to 5 sys

---
+++ rankVideo.py
@@ -0,0 +1,26 @@
+import json
+
+
+def returnStars(data):
+
+    data = json.loads(data)
+    content = data['fragments']
+
+    actualHappiness = 1
+    if (len(content) == 0):
+        print("The video must not be empty")
+
+    for x in range (0, len(content)):
+        for y in range(0, len(content[x])):
+           for z in range(0, len(content[x]['events'])):
+
+            happiness =  content[x]['events'][z][0]['windowMeanScores']['happiness'] if (content[x]['events'][z][0]['windowMeanScores']['happiness'] <= 1) else 0
+            sadness = content[x]['events'][z][0]['windowMeanScores']['sadness']  if (content[x]['events'][z][0]['windowMeanScores']['sadness'] <= 1) else 0
+            disgust = content[x]['events'][z][0]['windowMeanScores']['disgust'] if (content[x]['events'][z][0]['windowMeanScores']['disgust'] <= 1) else 0
+            fear = content[x]['events'][z][0]['windowMeanScores']['fear'] if (content[x]['events'][z][0]['windowMeanScores']['fear'] <= 1) else 0
+            neutral =content[x]['events'][z][0]['windowMeanScores']['neutral'] if (content[x]['events'][z][0]['windowMeanScores']['neutral'] <= 1) else 0
+            anger =content[x]['events'][z][0]['windowMeanScores']['anger'] if (content[x]['events'][z][0]['windowMeanScores']['anger'] <= 1) else 0
+            actualHappiness = (actualHappiness +
+                               happiness + (neutral/2) - (sadness + disgust + fear + anger))/2
+    #Let's make it more optimistic
+    return ((actualHappiness + 5)/2)*5
