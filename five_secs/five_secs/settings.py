dc770a9a1c78520cfe91d0c219554778ba24c75d
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2017-02-05 10:12:38 +0000

    Hack

--- five_secs/five_secs/settings.py
+++ five_secs/five_secs/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'api.middleware.MyMiddleware',
 ]
 
 ROOT_URLCONF = 'five_secs.urls'
